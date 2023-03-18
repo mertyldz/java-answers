5e2c5a30731544604f2a9f5306b8ee8b7645ee80
mertyldz <yzmert3@gmail.com>
2023-03-18 16:28:32 +0300

    leap year added

---
+++ leapYear/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Yıl Giriniz : ");
+        int year = scanner.nextInt();
+
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                System.out.println(year + " bir artık yıldır !");
+            } else {
+                System.out.println(year + " bir artık yıl değildir !");
+            }
+        } else {
+            if (year % 4 == 0) {
+                System.out.println(year + " bir artık yıldır !");
+            } else {
+                System.out.println(year + " bir artık yıl değildir !");
+            }
+        }
+    }
+}
