5258de76179a8e6f107ebe6f3a41e99c1cf236c9
mertyldz <yzmert3@gmail.com>
2023-03-21 22:20:44 +0300

    is duplicate added

---
+++ isDuplicate/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class Main {
+    static boolean isFind(int[] list, int value){
+        for (int i : list){
+            if(value==i){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] list = {3, 7, 3, 3, 2, 9, 10, 21, 1, 33, 9, 1};
+        int[] duplicate = new int[list.length];
+        int duplicateIndex=0;
+
+        for (int i = 0; i < list.length; i++) {
+            for(int j=0; j<list.length; j++){
+                if(i!=j && list[i] == list[j]){
+                    if(!isFind(duplicate, list[i])){
+                        duplicate[duplicateIndex++] = list[i];
+                    }
+                }
+            }
+        }
+        System.out.println(Arrays.toString(duplicate));
+    }
+}
